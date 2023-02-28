4d6a482063f4ef7ecbe68f457d48008cf6919321
Pramodh <pramodh123kit@gmail.com>
2023-02-28 19:11:18 +0530

    Website Header Design

---
+++ app.js
@@ -0,0 +1,7 @@
+const menu = document.getElementById("btn");
+const menuLinks = document.getElementById("light");
+
+menu.addEventListener('click',function(){
+  btn.classList.toggle("active");
+  light.classList.toggle("on");
+})
